3cd270a868f0057b09cf7c21834a4c86a914ea87
jmartinez419 <justynmartinez419@gmail.com>
2024-07-16 08:56:51 -0400

    first commit

---
+++ src/Bird.java
@@ -0,0 +1,8 @@
+public class Bird extends Animal{
+    void fly(){
+        System.out.println("I am Flying");
+    }
+    void singing(){
+        System.out.println("I am singing");
+    }
+}
---
+++ src/solution.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class solution {
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.singing();
+    }
+}
